0d548b0b685a67ae331b5e234dccc8a906d9648e
chancehardmanweber <chancehardman@mail.weber.edu>
2024-09-17 13:41:35 -0600

    created course model

---
+++ Models/Course.cs
@@ -0,0 +1,57 @@
+﻿using AsyncAcademy.Utils;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AsyncAcademy.Models;
+
+public class Course
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int CourseId { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    [Display(Name = "Course Name")]
+    public required string CourseName { get; set; }
+
+    [StringLength(60, MinimumLength = 1)]
+    [Required]
+    [Display(Name = "Class Name")]
+    public required string ClassName { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    [Display(Name = "Start Date")]
+    public DateTime StartDate { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    [Display(Name = "End Date")]
+    public DateTime EndDate { get; set; }
+
+    [Required]
+    [DataType(DataType.Time)]
+    [Display(Name = "Start Time")]
+    public DateTime StartTime { get; set; }
+
+    [Required]
+    [DataType(DataType.Time)]
+    [Display(Name = "End Time")]
+    public DateTime EndTime { get; set; }
+
+    [StringLength(60, MinimumLength = 1)]
+    [Required]
+    [Display(Name = "Location")]
+    public required string Location { get; set; }
+
+    [Required]
+    [Display(Name = "Student Capacity")]
+    public required int StudentCapacity { get; set; }
+
+    [Required]
+    [Display(Name = "Students Enrolled")]
+    public required int StudentsEnrolled { get; set; }
+
+}
+
