2f37f0482f760e62c3610ac71323dbe69a8c4d7e
hannawhitney <155977754+hannawhitney@users.noreply.github.com>
2024-09-20 19:31:49 -0600

    Changes to calendar functionality and addition of CalendarEvents

---
+++ Models/CalendarEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsyncAcademy.Models
+{
+    public class CalendarEvent
+    {
+
+        [Required(ErrorMessage = "Event title is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Event start time is required.")]
+        public DateTime StartTime { get; set; }
+
+        [Required(ErrorMessage = "Event end time is required.")]
+        public DateTime EndTime { get; set; }
+
+    }
+}
--- Pages/Calendar.cshtml.cs
+++ Pages/Calendar.cshtml.cs
@@ -1,7 +1,13 @@
 using AsyncAcademy.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using AsyncAcademy.Migrations;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
 
 namespace AsyncAcademy.Pages
 {
@@ -11,9 +17,17 @@ namespace AsyncAcademy.Pages
 
         [BindProperty]
         public User? Account { get; set; }
+         public List<Course> EnrolledCourses = [];
+        public List<Section> EnrolledSections = [];
+        public List<CalendarEvent> CalendarEvents = [];
+
+        public JsonResult result { get; set; }
+
+
 
         public async Task<IActionResult> OnGetAsync()
         {
+            //asigns user
             int? currentUserID = HttpContext.Session.GetInt32("CurrentUserId");
 
             if (currentUserID == null)
@@ -27,6 +41,34 @@ namespace AsyncAcademy.Pages
             {
                 return NotFound();
             }
+
+            // Get all corresponding classes for signed in user - Borrowed logic from Bash
+            var Enrollments = _context.Enrollments.ToList();
+            foreach (Enrollment e in Enrollments)
+            {
+                if (e.UserId == currentUserID) //fills list with sections signed in user is enrolled in
+                {
+                    Section? correspondingSection = _context.Sections.FirstOrDefault(a => a.Id == e.SectionId);
+                    if (correspondingSection == null)
+                    {
+                        BadRequest();
+                    }
+                    EnrolledSections.Add(correspondingSection);
+                }
+            }
+
+            //create calendar events for each section
+            //TO DO - NEED TO REFINE HOW CALENDAREVENT DATA IS COLLECTED TO MEET INFO REQUIREMENTS
+
+            foreach (Section s in EnrolledSections)
+            {
+                CalendarEvent NewEvent = new CalendarEvent();
+                NewEvent.Title = "Class"; // need to pull specific title
+                NewEvent.StartTime = s.StartTime;
+                NewEvent.EndTime = s.EndTime;
+                CalendarEvents.Add(NewEvent);
+            }
+
 
             return Page();
         }
