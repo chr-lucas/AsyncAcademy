52863fd0533ff3efe0992babf0aed4807dd00a95
chancehardmanweber <chancehardman@mail.weber.edu>
2024-09-24 13:55:49 -0600

    Students can no longer delete course

--- obj/Debug/net8.0/AsyncAcademy.AssemblyInfo.cs
+++ obj/Debug/net8.0/AsyncAcademy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AsyncAcademy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7968c81854ed6d4a7a044c4b8cdaebd04abf4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef70d08bae75ca1ec857beb365fddb313136ef9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AsyncAcademy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AsyncAcademy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
