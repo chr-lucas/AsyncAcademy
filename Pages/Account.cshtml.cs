fbf5580fde2fb63191725dac78d1f71ae966c75a
Alexmar97 <104046667+Alexmar97@users.noreply.github.com>
2024-09-25 22:00:46 -0600

    Added bare-bones Account page for now

---
+++ Pages/Account.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AsyncAcademy.Data;
+using AsyncAcademy.Models;
+
+namespace AsyncAcademy.Pages
+{
+    public class AccountModel : PageModel
+    {
+        private readonly AsyncAcademy.Data.AsyncAcademyContext _context;
+
+        public AccountModel(AsyncAcademy.Data.AsyncAcademyContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Enrollment Enrollment { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Enrollments.Add(Enrollment);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- obj/Debug/net8.0/AsyncAcademy.AssemblyInfo.cs
+++ obj/Debug/net8.0/AsyncAcademy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AsyncAcademy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0dc5dc2a5df7af3c1a71e7e392218afc00133a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693a9dda22b29c1dcfd4e4f7942a6dd0a0ad7d16")]
 [assembly: System.Reflection.AssemblyProductAttribute("AsyncAcademy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AsyncAcademy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
