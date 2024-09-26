95a34f96c5274d0f2af3e2ef8a7877cf28c52a62
hannawhitney <155977754+hannawhitney@users.noreply.github.com>
2024-09-26 10:44:03 -0600

    Assignments page and student functionality

---
+++ Pages/Assignments/Details.cshtml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AsyncAcademy.Data;
+using AsyncAcademy.Models;
+
+namespace AsyncAcademy.Pages.Assignments
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly AsyncAcademy.Data.AsyncAcademyContext _context;
+
+        public DetailsModel(AsyncAcademy.Data.AsyncAcademyContext context)
+        {
+            _context = context;
+        }
+
+        public Assignment Assignment { get; set; } = default!;
+
+        public User Account { get; set; }
+
+        public Course Course { get; set; }
+
+        [ViewData]
+        public string NavBarLink { get; set; }
+
+        [ViewData]
+        public string NavBarText { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+
+            //assigns user - Assisted by Chris L.
+            int? currentUserID = HttpContext.Session.GetInt32("CurrentUserId");
+
+            if (currentUserID == null)
+            {
+                return NotFound();
+            }
+
+            Account = await _context.Users.FirstOrDefaultAsync(a => a.Id == currentUserID);
+
+            if (Account == null)
+            {
+                return NotFound();
+            }
+
+            //sets navbar
+            if (Account.IsProfessor)
+            {
+                //WelcomeText = $"Welcome, Professor {firstname} {lastname}";
+                // Set ViewData variables for instructors
+                NavBarLink = "Course Pages/InstructorIndex"; // Set NavBarLink directly
+                NavBarText = "Classes"; // Set NavBarText directly
+            }
+            else
+            {
+                //WelcomeText = $"Welcome, {firstname} {lastname}";
+                NavBarLink = "Course Pages/StudentIndex"; // Set NavBarLink for non-professors
+                NavBarText = "Register"; // Set NavBarText for non-professors
+            }
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assignment = await _context.Assignment.FirstOrDefaultAsync(m => m.Id == id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Assignment = assignment;
+            }
+
+            //assigns course - Assisted by Kevin B.
+            Course = await _context.Course.FirstOrDefaultAsync(c => c.Id == assignment.CourseId);
+
+            if (Course == null)
+            {
+                NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/Assignments/Index.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AsyncAcademy.Data;
+using AsyncAcademy.Models;
+
+namespace AsyncAcademy.Pages.Assignments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AsyncAcademy.Data.AsyncAcademyContext _context;
+
+        public IndexModel(AsyncAcademy.Data.AsyncAcademyContext context)
+        {
+            _context = context;
+        }
+
+        public Course Course { get; set; }
+
+        [ViewData]
+        public string NavBarLink { get; set; } // Removed default initialization
+
+        [ViewData]
+        public string NavBarText { get; set; } // Removed default initialization
+
+        public User Account { get; set; }
+
+        public IList<Assignment> Assignment { get;set; }
+
+
+        public async Task OnGetAsync(int courseId)
+        {
+            //assigns course - Assisted by Kevin B.
+            Course = await _context.Course.FirstOrDefaultAsync(c => c.Id == courseId);
+
+            if (Course == null)
+            {
+                NotFound();
+            }
+
+            int? currentUserID = HttpContext.Session.GetInt32("CurrentUserId");
+
+            if (currentUserID == null)
+            {
+                NotFound();
+            }
+            //assigns user - Assisted by Chris L.
+            Account = await _context.Users.FirstOrDefaultAsync(a => a.Id == currentUserID);
+
+            if (Account == null)
+            {
+                NotFound();
+            }
+
+            //set navbar
+            if (Account.IsProfessor)
+            {
+                NavBarLink = "Course Pages/InstructorIndex";
+                NavBarText = "Classes";
+            }
+            else
+            {
+                NavBarLink = "Course Pages/StudentIndex"; 
+                NavBarText = "Register"; 
+            }
+
+            //Displays the assignments related to the course the user is viewing
+            var assignments = from a in _context.Assignment
+                              select a;
+            assignments = assignments.Where(a => a.CourseId == Course.Id);
+            Assignment = await assignments.ToListAsync();
+        }
+    }
+}
