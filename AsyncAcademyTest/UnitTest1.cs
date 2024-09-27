28a9edd0ddaa053352652e775cf7b1e786215fb6
chancehardmanweber <chancehardman@mail.weber.edu>
2024-09-26 21:22:58 -0600

    added in more steps

--- AsyncAcademyTest/UnitTest1.cs
+++ AsyncAcademyTest/UnitTest1.cs
@@ -8,9 +8,12 @@ namespace AsyncAcademyTest
         {
             //Start with known instructor id.
             //Find out how many course that user is teaching = m
+            //int m = SELECT COUNT(*) FROM course WHERE InstructorId = savedId;
             //Exercise the course creation functionality
             //Find out how many course that user is teaching = n
+            //int m = SELECT COUNT(*) FROM course WHERE InstructorId = savedId;
             //Assert n = m + 1
+            Assert.AreEqual((m+1), n);
         }
     }
 }
