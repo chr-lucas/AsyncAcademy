e3da3a368c79916227682e24ae2e745c59dde419
Alexmar97 <104046667+Alexmar97@users.noreply.github.com>
2024-10-04 20:12:09 -0600

    Started secure payment processing through Stripe's API

---
+++ AsyncAcademy/Controllers/CheckOutController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Stripe.Checkout;
+
+namespace AsyncAcademy.Controllers
+{
+    [Route("create-checkout-session")]
+    [ApiController]
+    public class CheckoutApiController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult> CreateCheckoutSession()
+        {
+            var body = await new System.IO.StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            var parsedBody = JObject.Parse(body);
+            var amount = parsedBody["amount"].Value<int>(); //Will read the amount field from JSON
+
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+            {
+                new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = amount * 100,
+                        Currency = "usd",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = "Tuition Due",
+                        },
+                    },
+                    Quantity = 1,
+                },
+            },
+                Mode = "payment",
+                SuccessUrl = "http://localhost:4242/success",  // Change URLs to your actual app
+                CancelUrl = "http://localhost:4242/cancel",
+            };
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            return Json(new { id = session.Id });
+        }
+    }
+
+}
--- AsyncAcademy/Program.cs
+++ AsyncAcademy/Program.cs
@@ -3,11 +3,13 @@ using Microsoft.Extensions.DependencyInjection;
 using AsyncAcademy.Data;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using AsyncAcademy.Models;
+using Stripe;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AsyncAcademyContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("FirstLastAppContext") ?? throw new InvalidOperationException("Connection string 'FirstLastAppContext' not found.")));
 
@@ -20,9 +22,20 @@ builder.Services.AddSession(options =>
 });
 //
 
+// Configure Stripe using secret key
+StripeConfiguration.ApiKey = "sk_test_51Q2OkdLffhLXA6EXPrfKh0VcjFq9OydxjlpNAE9tAFVygmAWLSrmKYjjwo2HliwAIicTtxat7XS5OLqAEMw1Ooce00JpCHPLkh";  // Secret key
+
+
 builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>(); // The Last Part
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 // Upon service creation, check logic for seeding an empty database
 using (var scope = app.Services.CreateScope())
@@ -50,5 +63,9 @@ app.UseAuthorization();
 app.UseSession(); // From Bash
 
 app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
