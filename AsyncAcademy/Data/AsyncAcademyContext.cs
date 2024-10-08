8a74d2dd4856d122412a3019ed218315f52bf34a
CLucas <chr.lucas@outlook.com>
2024-10-07 20:36:55 -0600

    Add Payments table to context

--- AsyncAcademy/Data/AsyncAcademyContext.cs
+++ AsyncAcademy/Data/AsyncAcademyContext.cs
@@ -14,11 +14,12 @@ namespace AsyncAcademy.Data
         {
         }
 
-        public DbSet<AsyncAcademy.Models.User> Users { get; set; } = default!;
-        public DbSet<AsyncAcademy.Models.Enrollment> Enrollments { get; set; } = default!;
-        public DbSet<AsyncAcademy.Models.Assignment> Assignment { get; set; } = default!;
-        public DbSet<AsyncAcademy.Models.Course> Course { get; set; } = default!;
-        public DbSet<AsyncAcademy.Models.Department> Department { get; set; } = default!;
+        public DbSet<User> Users { get; set; } = default!;
+        public DbSet<Enrollment> Enrollments { get; set; } = default!;
+        public DbSet<Assignment> Assignment { get; set; } = default!;
+        public DbSet<Course> Course { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
         public DbSet<Submission> Submissions { get; set; } = default!;
+        public DbSet<Payment> Payments { get; set; } = default!;
     }
 }
---
+++ AsyncAcademy/Models/Payment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AsyncAcademy.Models
+{
+    public class Payment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; } // Foreign key to link to User
+
+        [Required]
+        public decimal AmountPaid { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime Timestamp { get; set; }
+    }
+}
