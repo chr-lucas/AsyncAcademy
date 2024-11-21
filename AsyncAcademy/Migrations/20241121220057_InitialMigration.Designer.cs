7ca984cc5f1b9df502699a79c903d0ea0bf74f6c
CLucas <chr.lucas@outlook.com>
2024-11-21 15:01:30 -0700

    Re-Re-Reseed with corrected submission grades

--- AsyncAcademy/Migrations/20241121220057_InitialMigration.Designer.cs
+++ AsyncAcademy/Migrations/20241121220057_InitialMigration.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AsyncAcademy.Migrations
 {
     [DbContext(typeof(AsyncAcademyContext))]
-    [Migration("20241121215514_InitialMigration")]
+    [Migration("20241121220057_InitialMigration")]
     partial class InitialMigration
     {
         /// <inheritdoc />
