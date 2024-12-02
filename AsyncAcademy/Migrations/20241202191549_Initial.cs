8f56470e71faa30f58499785d0ec74311257d331
Sebastian Sato <sebastiansato@gmail.com>
2024-12-02 12:18:09 -0700

    Changed connection string back to titan DB
    More or less what it says in the title. Azure isn't working, so we had to go back to hosting locally and using the Titan DB.

--- AsyncAcademy/Migrations/20241202191549_Initial.cs
+++ AsyncAcademy/Migrations/20241202191549_Initial.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace AsyncAcademy.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialMigration : Migration
+    public partial class Initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
